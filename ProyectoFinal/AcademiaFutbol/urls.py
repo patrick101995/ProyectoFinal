f8ba93c466dbde02b49fd5ffc0f7e42506259e8e
patrick101995 <75943397+patrick101995@users.noreply.github.com>
2022-01-25 20:16:50 -0300

    Update urls.py
    Emparejando Patrick con Main

--- ProyectoFinal/AcademiaFutbol/urls.py
+++ ProyectoFinal/AcademiaFutbol/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
     path('', views.inicio, name="inicio"),
     path('altaJugador', views.altaJugador, name="AltaJugador"),
     path('altaGrupo', views.altaGrupo, name="AltaGrupo"),
+    path('grupos', views.GrupoList.as_view(), name="GrupoList"),
     path('buscar', views.buscar),
     path('formulario_jugador', views.registro_jugador, name="formulario_jugador"),
-    path('formulario_grupo', views.registro_grupo, name="formulario_grupo")
+    path('formulario_grupo', views.registro_grupo, name="formulario_grupo"),
+    path('contactoForm',views.contactoFormulario, name="contacto"),
+    path('contacto', views.ContactoList.as_view(), name="ContactoList"),
 ]
